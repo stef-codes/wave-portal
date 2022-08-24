5f494df91b8bdb41f716fca51d2d5f689c39efac
Stefanie <stefcodes@gmail.com>
2022-08-23 19:46:20 -0700

    fix syntax

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,18 +5,20 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves; 
+    
     constructor() {
         console.log("Making this Smart Contract AGAIN!!!");
     }
 
 
 function wave () public {
-    totalWaves += 1 
-    console.log("%s has waved!", msg.sender) 
+    totalWaves += 1; 
+    console.log("%s has waved!", msg.sender); 
 }
 
-function getTotal eeves() public views returns (uint256) {
-    console.logs("We have %d total waves!", totalWaves); 
+function getTotalWaves() public view returns (uint256) {
+    console.log("We have %d total waves!", totalWaves); 
     return totalWaves; 
 }
 }
